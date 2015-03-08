148dbebf672edf67c7abce76c2e9338dd6944e39
fanglu <fanglupku@gmail.com>
2015-03-07 23:27:28 -0800

    minor progress

--- baby.hs
+++ baby.hs
@@ -7,3 +7,5 @@ boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
 length' xs = sum [1 | _ <- xs]
 
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
+
+firstArray = [1,2,3,4]
--- module.hs
+++ module.hs
@@ -2,3 +2,5 @@ import Data.List
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+
